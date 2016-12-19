a562fad33ab68a74431ea6aed9ba0a8f45f13684
jbarron <jbvs.94@hotmail.fr>
2016-12-19 18:01:32 +0100

    ajout(fonctionnelle) des models images et utilisateur(grâce à devis)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :images
+  devise_for :utilisateurs
+  root to:"images#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161209145539) do
+
+  create_table "images", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "utilisateur_id"
+    t.boolean  "valider"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "images", ["utilisateur_id"], name: "index_images_on_utilisateur_id"
+
+  create_table "utilisateurs", force: :cascade do |t|
+    t.string   "login"
+    t.boolean  "admin"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+  end
+
+  add_index "utilisateurs", ["email"], name: "index_utilisateurs_on_email", unique: true
+  add_index "utilisateurs", ["reset_password_token"], name: "index_utilisateurs_on_reset_password_token", unique: true
 
 end
